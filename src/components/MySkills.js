473addb6f78bd4403510793836a24e638edb8256
hopecbangle <hopecbangle@gmail.com>
2021-03-21 18:07:49 -0600

    Buttons-Routing-WorkHistory-MySkills-Education

--- src/components/MySkills.js
+++ src/components/MySkills.js
@@ -7,20 +7,31 @@ function MySkills () {
     <div  className="App">
       <div className="container">
         <div className="text-center">
-          <h1>Here's a List of My Skills</h1>
+          <h1>Here is a List of My Skills</h1>
         </div>
-        
         <div className="row">
           <div className="col-sm">
-            
+            <h4>Microsoft Windows Operating Systems</h4>
+            <h4>Microsoft Office Suites</h4>
+            <h4>Adobe Acrobat</h4>
+            <h4>Adobe Dreamweaver</h4>
+            <h4>Adobe Photoshop</h4>
+            <h4>Adobe InDesign</h4>
+            <h4>Adobe Illustrator</h4>
           </div>
           <div className="col-sm">
-            
+            <h4>C++</h4>
+            <h4>C#</h4>
+            <h4>Allen Bradley PLC's</h4>
+            <h4>HTML</h4>
+            <h4>CSS</h4>
+            <h4>JavaScript</h4>
+            <h4>React</h4>
+            <h4>MongoDB</h4>
+            <h4>Express</h4>
+            <h4>Node.js</h4>
           </div>
-          <div className="col-sm">
-            
-          </div>
-        </div>
+        </div>  
       </div>
     </div>
   )};
